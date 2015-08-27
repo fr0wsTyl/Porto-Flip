56689e6960a4ec7c69bd231f7fddea511e96fc1a
pacho10 <pmpetkov1983@abv.bg>
2015-08-27 18:42:11 +0300

    add createCourse.html and createCourse.js

---
+++ iLearn/scripts/createCourse.js
@@ -0,0 +1,38 @@
+import {jquery} from 'jquery'
+import {Parse} from 'parse'
+import {parseInitialization} from 'scripts/engine.js'
+import {bootflat} from 'bootflat'
+
+function createCourse() {
+    parseInitialization();
+
+    $('#create-course-button').on('click', function() {
+        let courseType = 'open';
+        let $courseNameValue = $('#course-name').val();
+        let $isPrivate = $('#course-type').is(':checked');
+        if ($isPrivate) {
+            courseType = 'private';
+        }
+
+        let CourseObject = Parse.Object.extend('Course');
+        let currentCourse = new CourseObject();
+
+        currentCourse.save({
+            coursename: $courseNameValue,
+            coursetype: courseType
+        }, {
+            success: function(result) {
+                let $element = $('<div/ >').text('Successful create new course').addClass('label label-success').show();
+                $('#create-course-button').after($element);
+            },
+            error: function(err) {
+                let $element = $('<div/ >').text('Invalid data').addClass('label label-danger').show();
+                $('#create-course-button').after($element);
+            }
+        })
+    });
+}
+
+export {
+    createCourse
+}

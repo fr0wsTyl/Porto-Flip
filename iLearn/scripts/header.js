c7baed2d317d15a1ecc452b8bf195274b24a58fc
Chavdar Angelov <c.l.angelov@gmail.com>
2015-08-27 11:03:55 +0300

    Header.js now does not depend on the div id

--- iLearn/scripts/header.js
+++ iLearn/scripts/header.js
@@ -16,6 +16,12 @@ export {
 
 $('head').append('<link rel="stylesheet" href="css/header.css"/>');
 
-$.get( "content/header.html", function(data) {
-    $( "#wrapper" ).prepend(data);
+$.get("content/header.html", function(data) {
+
+    var $wrapper = $("#wrapper");
+    if ($wrapper.length === 0) {
+        $wrapper = $('<div/>').attr('id', 'wrapper');
+        $('body').prepend($wrapper);
+    }
+    $wrapper.prepend(data);
 });

f38a85f319ff8e13299863dc0857cc13566601f5
Chavdar Angelov <c.l.angelov@gmail.com>
2015-08-31 18:08:44 +0300

    After Logout redirecting to home screen

--- iLearn/scripts/logout.js
+++ iLearn/scripts/logout.js
@@ -2,13 +2,19 @@ import {jquery} from 'jquery'
 import {Parse} from 'parse'
 import {parseInitialization} from 'scripts/engine.js'
 
-function logout(element){
-parseInitialization();
 
-	$('#logout').on('click', function(){
-		Parse.User.logOut();
+var logout = new Promise(
+	function (element){
+		parseInitialization();
+
+		$('#logout').on('click', function(){
+			Parse.User.logOut();
+		});
+	})
+	.then(function() {
+		window.location.href = "index.html"
 	});
-}
+
 export {
 	logout
 }

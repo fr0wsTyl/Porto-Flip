8a7dadb6bd45a69f781ef67e98f154a3f3930b9a
fr0wsTyl <frowstyl@gmail.com>
2015-09-01 17:41:02 +0300

    Updated redirecting module /Added redirect from profile to the index/

--- iLearn/scripts/profile-redirect.js
+++ iLearn/scripts/profile-redirect.js
@@ -1,5 +1,16 @@
-(function () {
-	if (localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser') !== null) {
-		document.location.href = './profile.html';
+function profileRedirect(isLogged) {
+	if (isLogged) {
+		if (localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser') !== null) {
+			document.location.href = './profile.html';
+		}
 	}
-})();
+	else {
+		if (localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser') === null) {
+			document.location.href = './index.html';
+		}
+	}
+}
+
+export {
+	profileRedirect
+}

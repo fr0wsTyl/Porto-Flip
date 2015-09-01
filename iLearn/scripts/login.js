60b76bb55ccfca0f677194e1c25a296a06be916e
fr0wsTyl <frowstyl@gmail.com>
2015-09-02 00:53:19 +0300

    Added cool styles for login inputs / When data is wrong

--- iLearn/scripts/login.js
+++ iLearn/scripts/login.js
@@ -5,7 +5,7 @@ import {bootflat} from 'bootflat'
 
 function login(element) {
     parseInitialization();
-    const TIMEOUT_CHANING_PAGE = 1200;
+    const TIMEOUT_CHANING_PAGE = 1600;
     //$('#login-panel').load('./content/login-content.html');
     $('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
     $('#sign-in-button').on('click', function() {
@@ -18,12 +18,12 @@ function login(element) {
                 $('#sign-in-button').after($element);
                 $('#username-login-value').remove();
                 $('#password-login-value').remove();
-                let $elementLogin = $('<div class="form-group has-success has-feedback"><input type="text" class="form-control" id="login-success"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
-                let $elementPassword = $('<div class="form-group has-success has-feedback"><input type="password" class="form-control" id="password-success"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+                let $elementLogin = $('<div class="form-group has-success has-feedback"><input type="text" class="form-control" id="username-login-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+                let $elementPassword = $('<div class="form-group has-success has-feedback"><input type="password" class="form-control" id="password-login-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
                 $('.panel-body').prepend($elementLogin);
                 $($elementLogin).append($elementPassword);
-                $('#login-success').val($usernameValue);
-                $('#password-success').val($passwordValue);
+                $('#username-login-value').val($usernameValue);
+                $('#password-login-value').val($passwordValue);
                 setTimeout(function() {
                     document.location.href = 'profile.html';
                 }, TIMEOUT_CHANING_PAGE);
@@ -31,6 +31,14 @@ function login(element) {
             error: function(user, error) {
                 let $element = $('<div/ >').text('Invalid username or password').addClass('label label-danger').show();
                 $('#sign-in-button').after($element);
+                $('#username-login-value').remove();
+                $('#password-login-value').remove();
+                let $elementLogin = $('<div class="form-group has-error has-feedback"><input type="text" class="form-control" id="username-login-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+                let $elementPassword = $('<div class="form-group has-error has-feedback"><input type="password" class="form-control" id="password-login-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+                $('.panel-body').prepend($elementLogin);
+                $($elementLogin).append($elementPassword);
+                $('#username-login-value').val($usernameValue);
+                $('#password-login-value').val($passwordValue);
             }
         });
     });

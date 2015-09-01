60b862a696b71ab4e1b6ff628ad25315065f7512
fr0wsTyl <frowstyl@gmail.com>
2015-09-02 01:09:01 +0300

    Added fancy imput validations to registration.html

--- iLearn/scripts/registration.js
+++ iLearn/scripts/registration.js
@@ -6,7 +6,7 @@ import {validator} from 'scripts/validator.js'
 
 function registration() {
     parseInitialization();
-    const TIMEOUT_CHANGING_PAGE = 1500;
+    const TIMEOUT_CHANGING_PAGE = 1600;
     $('[data-toggle="tooltip"]').tooltip({ 'placement': 'top' });
 
     // TODO Make Valdiation with message in div
@@ -35,6 +35,25 @@ function registration() {
         let UserObject = Parse.Object.extend('User');
         let currentUser = new UserObject();
 
+        function throwError() {
+            $('#username-register-value').remove();
+            $('#email-register-value').remove();
+            $('#age-register-value').remove();
+            $('#password-register-value').remove();
+            let $elementRegister = $('<div class="form-group has-error has-feedback"><input type="text" class="form-control" id="username-register-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+            let $elementEmail = $('<div class="form-group has-error has-feedback"><input type="password" class="form-control" id="email-register-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+            let $elementAge = $('<div class="form-group has-error has-feedback"><input type="password" class="form-control" id="age-register-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+            let $elementPassword = $('<div class="form-group has-error has-feedback"><input type="password" class="form-control" id="password-register-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+            $('.panel-body').prepend($elementRegister);
+            $($elementRegister).append($elementEmail);
+            $elementEmail.append($elementAge);
+            $elementAge.append($elementPassword);
+            $('#username-register-value').val($usernameValue);
+            $('#email-register-value').val($emailValue);
+            $('#age-register-value').val($userAge);
+            $('#password-register-value').val($passwordValue);
+        }
+
         currentUser.save({
             username: $usernameValue,
             email: $emailValue,
@@ -47,6 +66,22 @@ function registration() {
                         success: function (user) {
                             let $element = $('<div/ >').text('Successful registration. Redirecting to your profile...').addClass('label label-success').show();
                             $('#result').html($element);
+                            $('#username-register-value').remove();
+                            $('#email-register-value').remove();
+                            $('#age-register-value').remove();
+                            $('#password-register-value').remove();
+                            let $elementRegister = $('<div class="form-group has-success has-feedback"><input type="text" class="form-control" id="username-register-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+                            let $elementEmail = $('<div class="form-group has-success has-feedback"><input type="password" class="form-control" id="email-register-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+                            let $elementAge = $('<div class="form-group has-success has-feedback"><input type="password" class="form-control" id="age-register-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+                            let $elementPassword = $('<div class="form-group has-success has-feedback"><input type="password" class="form-control" id="password-register-value"><span class="glyphicon glyphicon-ok form-control-feedback"></span></div>');
+                            $('.panel-body').prepend($elementRegister);
+                            $($elementRegister).append($elementEmail);
+                            $elementEmail.append($elementAge);
+                            $elementAge.append($elementPassword);
+                            $('#username-register-value').val($usernameValue);
+                            $('#email-register-value').val($emailValue);
+                            $('#age-register-value').val($userAge);
+                            $('#password-register-value').val($passwordValue);
                             setTimeout(function () {
                                 document.location.href = 'profile.html';
                             }, TIMEOUT_CHANGING_PAGE);
@@ -54,12 +89,14 @@ function registration() {
                         error: function (user, error) {
                             let $element = $('<div/ >').text('Something happened').addClass('label label-danger').show();
                             $('#result').html($element);
+                            throwError();
                         }
                     });
                 },
                 error: function (err) {
                     let $element = $('<div/ >').text('Invalid data').addClass('label label-danger').show();
                     $('#result').html($element);
+                    throwError();
                 }
             })
 

8a7b3f4496733071dfdab8c8d89f1de4d2797f65
Chavdar Angelov <c.l.angelov@gmail.com>
2015-09-01 20:25:59 +0300

    Get User Name now greets you in two different ways depend whether you are a Teacher or not

--- iLearn/scripts/getusername.js
+++ iLearn/scripts/getusername.js
@@ -1,6 +1,15 @@
 // use Parse Application ID Key to get info: /Parse/key/currentUser
-var currentUser = JSON.parse(localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser'));
+(function(){
+    var currentUser = JSON.parse(localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser'));
 
-var name = currentUser.username;
+    var name = currentUser.username;
+    var isTeacher = currentUser.isTeacher;
 
-$('<h3 />').text('Hi, ' + name).prependTo('#main');
+    if (isTeacher) {
+        $('<h3 />').text('Dear, ' + name).prependTo('#main');
+    } else {
+        $('<h3 />').text('Hi, ' + name).prependTo('#main');
+    }
+}());
+
+

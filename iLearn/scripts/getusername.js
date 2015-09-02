60c5279582937c74a17362eff4460ad93e3b33b6
fr0wsTyl <frowstyl@gmail.com>
2015-09-02 15:56:40 +0300

    Changed getusername module - now has 2 different methods and is much more abstract

--- iLearn/scripts/getusername.js
+++ iLearn/scripts/getusername.js
@@ -1,15 +1,25 @@
 // use Parse Application ID Key to get info: /Parse/key/currentUser
-(function(){
+
+
+function isTeacher() {
     var currentUser = JSON.parse(localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser'));
 
-    var name = currentUser.username;
+    //var name = currentUser.username;
     var isTeacher = currentUser.isTeacher;
+            
+    return isTeacher;
+}
 
-    if (isTeacher) {
-        $('<h3 />').text('Dear, ' + name + ' YOU ARE A TEACHER').prependTo('#main');
+function sayHello() {
+    if (isTeacher()) {
+        $('<h3 />').text('Hello! -@YOU ARE A TEACHER').prependTo('#main');
     } else {
-        $('<h3 />').text('Hi, ' + name + ' YOU ARE A STUDENT').prependTo('#main');
+        $('<h3 />').text('Hello! -@YOU ARE A STUDENT').prependTo('#main');
     }
-}());
+}
 
+export {
+    isTeacher,
+    sayHello
+}
 

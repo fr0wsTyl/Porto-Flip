361fbfc4ba0191c31602cf2b954357d18abbaff7
Chavdar Angelov <c.l.angelov@gmail.com>
2015-09-03 17:04:08 +0300

    edit profile is working when loaded with sammy.js

--- iLearn/scripts/edit-profile.js
+++ iLearn/scripts/edit-profile.js
@@ -5,95 +5,97 @@ import {parseInitialization} from 'scripts/engine.js';
 import {validator} from 'scripts/validator.js'
 import {handleError} from 'scripts/error-handler.js';
 
-(function () {
-	parseInitialization();
-	$('[data-toggle="tooltip"]').tooltip({ 'placement': 'top' });
+export function edit () {
+	$('#content').load('./content/edit-profile.html', function () {
+		parseInitialization();
+		$('[data-toggle="tooltip"]').tooltip({ 'placement': 'top' });
 
-	let $emailInput = $('#email-edit-value');
-	let $ageInput = $('#age-edit-value');
-	let $passwordInput = $('#password-edit-value');
-	let $saveChanges = $('#save-changes-button');
+		let $emailInput = $('#email-edit-value');
+		let $ageInput = $('#age-edit-value');
+		let $passwordInput = $('#password-edit-value');
+		let $saveChanges = $('#save-changes-button');
 
-	let currentUser = localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser');
-	let username = JSON.parse(currentUser).username;
-	let User = Parse.Object.extend("User");
-	let query = new Parse.Query(User);
-	query.equalTo("username", username);
-	query.find({
-		success: function (results) {
-			$emailInput.val(results[0].attributes.email);
-			$ageInput.val(results[0].attributes.age);
-		},
-		error: function (err) {
-			console.log(err);
-		}
-	});
+		let currentUser = localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser');
+		let username = JSON.parse(currentUser).username;
+		let User = Parse.Object.extend("User");
+		let query = new Parse.Query(User);
+		query.equalTo("username", username);
+		query.find({
+			success: function (results) {
+				$emailInput.val(results[0].attributes.email);
+				$ageInput.val(results[0].attributes.age);
+			},
+			error: function (err) {
+				console.log(err);
+			}
+		});
 
-	$saveChanges.on('click', function () {
-		function successChange(isPasswordChanged) {
-			if (isPasswordChanged) {
-				let $element = $('<div/ >').text('Changes were saved. You will need to sign in again...').addClass('label label-success').show();
+		$saveChanges.on('click', function () {
+			function successChange(isPasswordChanged) {
+				if (isPasswordChanged) {
+					let $element = $('<div/ >').text('Changes were saved. You will need to sign in again...').addClass('label label-success').show();
+					$('#result').html($element);
+					setTimeout(function () {
+						document.location.href = './login.html';
+						Parse.User.logOut();
+						localStorage.clear();
+					}, 2500);
+				}
+				else {
+					let $element = $('<div/ >').text('Changes were saved.').addClass('label label-success').show();
+					$('#result').html($element);
+				}
+			}
+			function changeFail(err) {
+				let $element = $('<div/ >').text('Invalid data').addClass('label label-danger').show();
 				$('#result').html($element);
-				setTimeout(function () {
-					document.location.href = './login.html';
-					Parse.User.logOut();
-					localStorage.clear();
-				}, 2500);
+				console.log(err);
 			}
-			else {
-				let $element = $('<div/ >').text('Changes were saved.').addClass('label label-success').show();
-				$('#result').html($element);
-			}
-		}
-		function changeFail(err) {
-			let $element = $('<div/ >').text('Invalid data').addClass('label label-danger').show();
-			$('#result').html($element);
-			console.log(err);
-		}
-		let $emailInputValue = $('#email-edit-value').val();
-		let $ageInputValue = $('#age-edit-value').val();
-		let $passwordInputValue = $('#password-edit-value').val();
+			let $emailInputValue = $('#email-edit-value').val();
+			let $ageInputValue = $('#age-edit-value').val();
+			let $passwordInputValue = $('#password-edit-value').val();
 
-		try {
-			validator.validateUserMail($emailInputValue);
-			validator.validateUserAge($ageInputValue);
-			if ($passwordInputValue) {
-				validator.validateUserPasswordLength($passwordInputValue);
+			try {
+				validator.validateUserMail($emailInputValue);
+				validator.validateUserAge($ageInputValue);
+				if ($passwordInputValue) {
+					validator.validateUserPasswordLength($passwordInputValue);
+				}
+			} catch (err) {
+				if (err.name === 'InputMailError') {
+					handleError('#email-edit-value', err.message, $emailInputValue);
+				}
+				if (err.name === 'InputAgeError') {
+					handleError('#age-edit-value', err.message, $ageInputValue);
+				}
+				if (err.name === 'InputPasswordLengthError') {
+					handleError('#password-edit-value', err.message, $passwordInputValue);
+				}
+				return
 			}
-		} catch (err) {
-			if (err.name === 'InputMailError') {
-				handleError('#email-edit-value', err.message, $emailInputValue);
-			}
-			if (err.name === 'InputAgeError') {
-				handleError('#age-edit-value', err.message, $ageInputValue);
-			}
-			if (err.name === 'InputPasswordLengthError') {
-				handleError('#password-edit-value', err.message, $passwordInputValue);
-			}
-			return
-		}
 
-		var currentUser = Parse.User.current();
-		if (currentUser) {
-			if ($passwordInputValue.length > 0) {
-				currentUser.save({
-					email: $emailInputValue,
-					password: $passwordInputValue,
-					age: parseInt($ageInputValue)
-				}, {
+			var currentUser = Parse.User.current();
+			if (currentUser) {
+				if ($passwordInputValue.length > 0) {
+					currentUser.save({
+						email: $emailInputValue,
+						password: $passwordInputValue,
+						age: parseInt($ageInputValue)
+					}, {
 						success: successChange(true)
 					});
-			}
-			else {
-				currentUser.save({
-					email: $emailInputValue,
-					age: parseInt($ageInputValue)
-				}, {
+				}
+				else {
+					currentUser.save({
+						email: $emailInputValue,
+						age: parseInt($ageInputValue)
+					}, {
 						success: successChange(false)
 					});
+				}
+
 			}
+		});
 
-		}
 	});
-
-})();
+}
--- iLearn/scripts/routes.js
+++ iLearn/scripts/routes.js
@@ -1,18 +1,12 @@
 import {jquery} from 'jquery';
+import {edit} from 'scripts/edit-profile.js'
 
 (function () {
 	var sammyApp = Sammy('#content', function () {
 		this.get('#/', function () {
 			$('#content').html('');
 		});
-		this.get('#/edit-profile', function () {
-			$('#content').load('./content/edit-profile.html');
-			// var fileref = document.createElement('<script>');
-			// fileref.setAttribute("id", "edit-profile-script");
-			// fileref.setAttribute("type", "text/javascript");
-			// fileref.setAttribute("src", 'scripts/edit-profile.js');
-			// $('body').append(fileref);
-		});
+		this.get('#/edit-profile', edit);
 	});
 	sammyApp.run('#/');
 })();

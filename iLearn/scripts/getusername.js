8ef767ba1806a01af680884daecbc2d60eb799b7
Chavdar Angelov <c.l.angelov@gmail.com>
2015-09-03 14:37:13 +0300

    Navigation is loaded depending the user type

--- iLearn/scripts/getusername.js
+++ iLearn/scripts/getusername.js
@@ -10,16 +10,16 @@ function isTeacher() {
     return isTeacher;
 }
 
-// function sayHello() {
-//     if (isTeacher()) {
-//         $('<h3 />').text('Hello! -@YOU ARE A TEACHER').prependTo('#main');
-//     } else {
-//         $('<h3 />').text('Hello! -@YOU ARE A STUDENT').prependTo('#main');
-//     }
-// }
+function sayHello() {
+    if (isTeacher()) {
+        $('<h3 />').text('Hello! -@YOU ARE A TEACHER').css('text-align', 'center').insertBefore('#content');
+    } else {
+        $('<h3 />').text('Hello! -@YOU ARE A STUDENT').css('text-align', 'center').insertBefore('#content');
+    }
+}
 
 export {
     isTeacher,
-    // sayHello
+    sayHello
 }
 
---
+++ iLearn/scripts/navigation-handler.js
@@ -0,0 +1,21 @@
+var currentUser = JSON.parse(localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser'));
+var isTeacher = currentUser.isTeacher;
+var isLogged = (localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser') !== null);
+
+if (!isLogged) {
+    makeAjaxRequest ("content/nav-home.html");
+} else if (isTeacher) {
+    makeAjaxRequest ("content/nav-teacher.html");
+} else {
+    makeAjaxRequest ("content/nav-student.html");
+}
+
+function makeAjaxRequest (url) {
+    $.ajax(url, {
+        type: 'GET',
+        contentType: 'text/html',
+        success: function (data) {
+            $('#nav').html('').append(data)
+        }});
+}
+

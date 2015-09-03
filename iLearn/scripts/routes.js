5a506ef7161ae8ab2dd6928619a347a20161543b
fr0wsTyl <frowstyl@gmail.com>
2015-09-03 12:28:23 +0300

    Added sammy

--- iLearn/scripts/routes.js
+++ iLearn/scripts/routes.js
@@ -1,5 +1,15 @@
-import {Sammy} from 'sammy';
+import {jquery} from 'jquery';
+// import {Sammy} from 'scripts/libs/sammy-latest.min.js';
 
 (function () {
+	console.log(sammy);
+	var sammy = Sammy('#content', function () {
+		
+		this.get('#/', function () {
+			console.log('here');
+		});
+	})
+	
+	sammy.run('#/');
 	
 })();

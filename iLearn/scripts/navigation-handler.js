32f5ea1ef542c868020233037b8bfb45b7dd3920
Chavdar Angelov <c.l.angelov@gmail.com>
2015-09-03 17:31:12 +0300

    Navigation handler now works proper when no user is logged

--- iLearn/scripts/navigation-handler.js
+++ iLearn/scripts/navigation-handler.js
@@ -1,6 +1,10 @@
-var currentUser = JSON.parse(localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser'));
-var isTeacher = currentUser.isTeacher;
+
 var isLogged = (localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser') !== null);
+
+if (isLogged) {
+    var currentUser = JSON.parse(localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser'));
+    var isTeacher = currentUser.isTeacher;
+}
 
 if (!isLogged) {
     makeAjaxRequest ("content/nav-home.html");

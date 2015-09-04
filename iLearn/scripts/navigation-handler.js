e459a21a7547258dac6af9e41fd6bc48bdf0ba3b
Chavdar Angelov <c.l.angelov@gmail.com>
2015-09-04 14:01:34 +0300

    navigation-handler is now an iife

--- iLearn/scripts/navigation-handler.js
+++ iLearn/scripts/navigation-handler.js
@@ -1,25 +1,27 @@
+(function () {
+    var isLogged = (localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser') !== null);
 
-var isLogged = (localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser') !== null);
+    if (isLogged) {
+        var currentUser = JSON.parse(localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser'));
+        var isTeacher = currentUser.isTeacher;
+    }
 
-if (isLogged) {
-    var currentUser = JSON.parse(localStorage.getItem('Parse/OxNzrzXTEVzRxH9qHpel84j4dD8QJs4aFUbmrWYc/currentUser'));
-    var isTeacher = currentUser.isTeacher;
-}
+    if (!isLogged) {
+        makeAjaxRequest("content/nav-home.html");
+    } else if (isTeacher) {
+        makeAjaxRequest("content/nav-teacher.html");
+    } else {
+        makeAjaxRequest("content/nav-student.html");
+    }
 
-if (!isLogged) {
-    makeAjaxRequest("content/nav-home.html");
-} else if (isTeacher) {
-    makeAjaxRequest("content/nav-teacher.html");
-} else {
-    makeAjaxRequest("content/nav-student.html");
-}
+    function makeAjaxRequest (url) {
+        $.ajax(url, {
+            type: 'GET',
+            contentType: 'text/html',
+            success: function (data) {
+                $('#nav').html('').append(data)
+            }});
+    }
+} ());
 
-function makeAjaxRequest (url) {
-    $.ajax(url, {
-        type: 'GET',
-        contentType: 'text/html',
-        success: function (data) {
-            $('#nav').html('').append(data)
-        }});
-}
 
